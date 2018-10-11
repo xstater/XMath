3f24b484355954d372e8da0bb63355ee778832ef
xstater <a166224@163.com>
2018-10-11 21:19:15 +0800

    IDE自动更新

---
+++ cmake-build-debug/CMakeFiles/XMath.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/XMath.dir/test2.cpp.obj"
+  "XMath.pdb"
+  "XMath.exe"
+  "XMath.exe.manifest"
+  "libXMath.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/XMath.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/XMath.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for XMath.
+# This may be replaced when dependencies are built.
