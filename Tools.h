6b946aeae274a78c0f31f23d84d8e709b35285d1
xstater <a166224@163.com>
2018-12-18 13:51:24 +0800

    修改了排版

--- Tools.h
+++ Tools.h
@@ -59,39 +59,39 @@ namespace xmath{
     template <class Type>
     Matrix4d<Type> perspective(Type top,Type buttom,Type left,Type right,Type znear,Type zfar){
         return Matrix4d<Type>{
-            2.0f*znear/(right-left),                  0.0,(right+left)/(right-left),                    0.0,
-                              0.0,2.0f*znear/(top-buttom),(top+buttom)/(top-buttom),                    0.0,
-                              0.0,                  0.0,(zfar+znear)/(znear-zfar),2.0f*zfar*znear/(znear-zfar),
-                              0.0,                  0.0,                     -1.0,                    0.0
+            static_cast<Type>(2.0)*znear/(right-left),                                      0.0,(right+left)/(right-left),                                           0.0,
+                                                  0.0,static_cast<Type>(2.0)*znear/(top-buttom),(top+buttom)/(top-buttom),                                           0.0,
+                                                  0.0,                                      0.0,(zfar+znear)/(znear-zfar),static_cast<Type>(2.0)*zfar*znear/(znear-zfar),
+                                                  0.0,                                      0.0,                     -1.0,                                           0.0
         };
     }
 
     template <class Type>
     Matrix4d<Type> perspective(Type width,Type height,Type znear,Type zfar){
         return Matrix4d<Type>{
-                2.0f*znear/width,              0.0,                      0.0,                         0.0,
-                             0.0,2.0f*znear/height,                      0.0,                         0.0,
-                             0.0,              0.0,(znear+zfar)/(znear-zfar),2.0f*znear*zfar/(znear-zfar),
-                             0.0,              0.0,                     -1.0,                         0.0
+            static_cast<Type>(2.0)*znear/width,                                0.0,                      0.0,                                           0.0,
+                                           0.0,static_cast<Type>(2.0)*znear/height,                      0.0,                                           0.0,
+                                           0.0,                                0.0,(znear+zfar)/(znear-zfar),static_cast<Type>(2.0)*znear*zfar/(znear-zfar),
+                                           0.0,                                0.0,                     -1.0,                                           0.0
         };
     }
 
     template <class Type>
     Matrix4d<Type> perspective_fov(Type fov,Type aspect,Type znear,Type zfar){
         return Matrix4d<Type>{
-                1.0f/(std::tan(fov/2.0f)*aspect),                    0.0,                      0.0,                         0.0,
-                                           0.0,1.0f/std::tan(fov/2.0f),                      0.0,                         0.0,
-                                           0.0,                    0.0,(zfar+znear)/(znear-zfar),(2.0f*zfar*znear)/(znear-zfar),
-                                           0.0,                    0.0,                     -1.0,                         0.0
+            static_cast<Type>(1.0)/(std::tan(fov/static_cast<Type>(2.0))*aspect),0.0,0.0,0.0,
+            0.0,static_cast<Type>(1.0)/std::tan(fov/ static_cast<Type>(2.0)),0.0,0.0,
+            0.0,0.0,(zfar+znear)/(znear-zfar),(static_cast<Type>(2.0)*zfar*znear)/(znear-zfar),
+            0.0,0.0,-1.0,0.0
         };
     }
 
     template <class Type>
     Matrix4d<Type> ortho(Type top,Type buttom,Type left,Type right,Type znear,Type zfar){
         return Matrix4d<Type>{
-            2.0f/(right-left),              0.0,              0.0,(left+right)/(left-right),
-                          0.0,2.0f/(top-buttom),              0.0,(buttom+top)/(buttom-top),
-                          0.0,              0.0,2.0f/(znear-zfar),(znear+zfar)/(znear-zfar),
+                static_cast<Type>(2.0)/(right-left),              0.0,              0.0,(left+right)/(left-right),
+                          0.0,static_cast<Type>(2.0)/(top-buttom),              0.0,(buttom+top)/(buttom-top),
+                          0.0,              0.0, static_cast<Type>(2.0)/(znear-zfar),(znear+zfar)/(znear-zfar),
                           0.0,              0.0,              0.0,                      1.0
         };
     }
@@ -99,9 +99,9 @@ namespace xmath{
     template <class Type>
     Matrix4d<Type> ortho(Type width,Type height,Type znear,Type zfar){
         return Matrix4d<Type>{
-                2.0f/width,        0.0,              0.0,                      0.0,
-                       0.0,2.0f/height,              0.0,                      0.0,
-                       0.0,        0.0,2.0f/(znear-zfar),(znear+zfar)/(znear-zfar),
+                static_cast<Type>(2.0)/width,        0.0,              0.0,                      0.0,
+                       0.0,static_cast<Type>(2.0)/height,              0.0,                      0.0,
+                       0.0,        0.0, static_cast<Type>(2.0)/(znear-zfar),(znear+zfar)/(znear-zfar),
                        0.0,        0.0,              0.0,                     1.0
         };
     }
